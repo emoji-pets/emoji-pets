e5d9969ac61933b0ffb7831362aebeef5d8010f4
lracca1 <lracca@cnm.edu>
2018-07-30 10:01:35 -0600

    Added new activity for new game button and help button.

--- app/src/main/java/edu/cnm/deepdive/emojipets/GameMenuActivity.java
+++ app/src/main/java/edu/cnm/deepdive/emojipets/GameMenuActivity.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.emojipets;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -8,14 +9,26 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class GameMenuActivity extends AppCompatActivity {
+
+  private Button newGame;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_game_menu);
+
+    newGame = findViewById(R.id.imageButton);
+    newGame.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent i = new Intent(GameMenuActivity.this, NewGameActivity.class);
+        startActivity(i);
+      }
+    });
 
     ConstraintLayout constraintLayout = findViewById(R.id.layout);
     AnimationDrawable animationDrawable = (AnimationDrawable)constraintLayout.getBackground();
@@ -29,6 +42,8 @@ public class GameMenuActivity extends AppCompatActivity {
       @Override
       public void onClick(View v) {
         imageButton3.animate().rotation(imageButton3.getRotation()-360).start();
+        Intent i = new Intent(GameMenuActivity.this, HelpActivity.class);
+        startActivity(i);
       }
     });
 
