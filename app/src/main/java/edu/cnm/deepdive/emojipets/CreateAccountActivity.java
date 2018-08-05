afa00850f5d977163990baf5f029ef6ac42a458f
nicklocicero <nicklocicero@gmail.com>
2018-08-05 17:08:55 -0600

    I added a back button for going back to the SignInActivity from CreateAccountActivity. It logs the person out upon backpress, too.

--- app/src/main/java/edu/cnm/deepdive/emojipets/CreateAccountActivity.java
+++ app/src/main/java/edu/cnm/deepdive/emojipets/CreateAccountActivity.java
@@ -2,15 +2,19 @@ package edu.cnm.deepdive.emojipets;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.emojipets.pojo.Player;
@@ -33,6 +37,34 @@ public class CreateAccountActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_create_account);
+//    getActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//    if (getSupportActionBar() != null) {
+//      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//      getSupportActionBar().setDisplayShowHomeEnabled(true);
+//    }
+
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+    if (getSupportActionBar() != null) {
+      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+      getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        EmojiPetApplication application = EmojiPetApplication.getInstance();
+        application.getSignInClient().signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+          @Override
+          public void onComplete(@NonNull Task<Void> task) {
+            EmojiPetApplication.getInstance().setSignInAccount(null);
+          }
+        });
+        finish();
+      }
+    });
 
     setupServices();
 
